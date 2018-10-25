80747f8043018ebaf303e034357971cddbcc1193
Álvaro Saúl Rodríguez Alemán <>
2018-10-25 10:15:16 +0100

    Kata4 v2.0

--- src/main/Kata4.java
+++ src/main/Kata4.java
@@ -3,6 +3,7 @@ package main;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import model.Histogram;
 import model.Mail;
 import view.HistogramDisplay;
 import view.MailHistogramBuilder;
@@ -10,14 +11,34 @@ import view.MailListReader;
 
 public class Kata4 {
 
+    Histogram histogram;
+    List<Mail> arr;
+    
     public static void main(String[] args) {
-        List<Mail> arr = new ArrayList<Mail>();
+        Kata4 kata4 = new Kata4();
+        kata4.control();
+    }
+    
+    private void control() {
+        input();
+        process();
+        output();
+    }
+
+    private void input() {
+        arr = new ArrayList<Mail>();
         try {
             arr = new MailListReader().read("email.txt");
         } catch (IOException ex) {
             System.out.println("No se pudo leer el fichero: " + ex);
         }
-        new HistogramDisplay(new MailHistogramBuilder().build(arr)).execute();
+    }
+
+    private void process() {
+        histogram = new MailHistogramBuilder().build(arr);
     }
     
+    private void output() {
+        new HistogramDisplay(histogram).execute();
+    }
 }
